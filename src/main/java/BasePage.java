cc6d9c39ac55be1aecb518001f4a22d172a1f2a5
abmetko.a <abmetko.a@gmail.com>
2021-10-20 14:23:17 +0300

    test

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -4,20 +4,20 @@ public abstract class BasePage {
 
     public SelenideElement COMPONENT_CONTAINER;
 
-    public BasePage(SelenideElement component_container){
+    public BasePage(SelenideElement component_container) {
         this.COMPONENT_CONTAINER = component_container;
     }
 
-    public void waitPageOpen(){
+    public void waitPageOpen() {
         COMPONENT_CONTAINER
                 .shoulBe(Condition.visible, Duration.ofSeconds(30));
     }
 
-    public boolean waitIsPageOpen(){
-        try{
+    public boolean waitIsPageOpen() {
+        try {
             waitPageOpen();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
